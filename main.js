6d6ba59591e9f51610b24dc62574975a7f471543
Fayzulin694 <abdujalilovfayzullo0407@gmail.com>
2023-05-09 18:56:09 +0500

    lesson 6 homework js

---
+++ main.js
@@ -0,0 +1,26 @@
+let elResult = document.querySelector('.title')
+
+const money = prompt('Summani kiriting')
+console.log(money)
+
+let dollar = 11000.34;
+let yevro = 12345.03;
+
+let hotel = 250;
+let bilet = 500;
+let muzey = 120;
+
+console.log(250 * dollar);
+console.log(500 * dollar);
+console.log(120 * yevro);
+console.log(2750085 + 5500170 + 1481403.3)
+
+
+if(money >= 9731658.3){
+    elResult.textContent = 'Alisher yaxshi yetip ol'
+}else{
+    elResult.textContent = 'Alisher ozgina sabr qilish kerak ekan'
+}
+
+
+
